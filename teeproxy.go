98af6dcfaf6bb20ed5246b72f6ad2ae9188699ec
Justin Wilson <justin@minty.io>
2017-04-28 16:42:26 -0600

    Refactored for a more specific use case. Removed TLS support _(maybe add this back later)_. Will be using TLS upstream of this proxy. This also has the added benefit of not requiring TLS libs to be installed _(scratch image)_.
    Thinking of allowing multiple alternates to mirror traffic to.

--- teeproxy.go
+++ teeproxy.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"bytes"
-	"crypto/tls"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"math/rand"
+	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -15,179 +14,126 @@ import (
 	"time"
 )
 
-// Console flags
 var (
-	listen                = flag.String("l", ":8888", "port to accept requests")
-	targetProduction      = flag.String("a", "localhost:8080", "where production traffic goes. http://localhost:8080/production")
-	altTarget             = flag.String("b", "localhost:8081", "where testing traffic goes. response are skipped. http://localhost:8081/test")
-	debug                 = flag.Bool("debug", false, "more logging, showing ignored output")
-	productionTimeout     = flag.Int("a.timeout", 3, "timeout in seconds for production traffic")
-	alternateTimeout      = flag.Int("b.timeout", 1, "timeout in seconds for alternate site traffic")
-	productionHostRewrite = flag.Bool("a.rewrite", false, "rewrite the host header when proxying production traffic")
-	alternateHostRewrite  = flag.Bool("b.rewrite", false, "rewrite the host header when proxying alternate site traffic")
-	percent               = flag.Float64("p", 100.0, "float64 percentage of traffic to send to testing")
-	tlsPrivateKey         = flag.String("key.file", "", "path to the TLS private key file")
-	tlsCertificate        = flag.String("cert.file", "", "path to the TLS certificate file")
+	listen               = flag.String("l", ":8080", "port to accept requests")
+	targetHost           = flag.String("a", "localhost:8080", "where target traffic goes. http://localhost:8080/")
+	alternateHost        = flag.String("b", "localhost:8081", "where alternate traffic goes, response is ignored. http://localhost:8081/")
+	targetTimeout        = flag.Int("a.timeout", 3, "timeout in seconds for target traffic")
+	alternateTimeout     = flag.Int("b.timeout", 3, "timeout in seconds for alternate site traffic")
+	targetHostRewrite    = flag.Bool("a.rewrite", false, "rewrite the host header when proxying target traffic")
+	alternateHostRewrite = flag.Bool("b.rewrite", false, "rewrite the host header when proxying alternate site traffic")
 )
 
-// handler contains the address of the main Target and the one for the Alternative target
-type handler struct {
-	Target      string
-	Alternative string
-	Randomizer  rand.Rand
+type noopCloser struct {
+	io.Reader
 }
 
-// ServeHTTP duplicates the incoming request (req) and does the request to the Target and the Alternate target discading the Alternate response
-func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	var productionRequest, alternativeRequest *http.Request
-	if *percent == 100.0 || h.Randomizer.Float64()*100 < *percent {
-		alternativeRequest, productionRequest = DuplicateRequest(req)
-		go func() {
-			defer func() {
-				if r := recover(); r != nil && *debug {
-					fmt.Println("Recovered in f", r)
-				}
-			}()
-			// Open new TCP connection to the server
-			clientTcpConn, err := net.DialTimeout("tcp", h.Alternative, time.Duration(time.Duration(*alternateTimeout)*time.Second))
-			if err != nil {
-				if *debug {
-					fmt.Printf("Failed to connect to %s\n", h.Alternative)
-				}
-				return
-			}
-			clientHttpConn := httputil.NewClientConn(clientTcpConn, nil) // Start a new HTTP connection on it
-			defer clientHttpConn.Close()                                 // Close the connection to the server
-			if *alternateHostRewrite {
-				alternativeRequest.Host = h.Alternative
-			}
-			err = clientHttpConn.Write(alternativeRequest) // Pass on the request
-			if err != nil {
-				if *debug {
-					fmt.Printf("Failed to send to %s: %v\n", h.Alternative, err)
-				}
-				return
-			}
-			_, err = clientHttpConn.Read(alternativeRequest) // Read back the reply
-			if err != nil && err != httputil.ErrPersistEOF {
-				if *debug {
-					fmt.Printf("Failed to receive from %s: %v\n", h.Alternative, err)
-				}
-				return
-			}
-		}()
-	} else {
-		productionRequest = req
+func (noopCloser) Close() error {
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var err error
+	var listener net.Listener
+
+	listener, err = net.Listen("tcp", *listen)
+	if err != nil {
+		fmt.Printf("Failed to listen to %s: %s\n", *listen, err)
+		return
 	}
-	defer func() {
-		if r := recover(); r != nil && *debug {
-			fmt.Println("Recovered in f", r)
+	fmt.Printf("Listening on %s\n", *listen)
+	http.Serve(listener, http.HandlerFunc(handler))
+}
+
+func recovery() {
+	if r := recover(); r != nil {
+		log.Println("Unsafe recovery: ", r)
+	}
+}
+
+// handler duplicates the incoming request (req) and does the request to the Target and the Alternate target discarding the Alternate response
+func handler(w http.ResponseWriter, req *http.Request) {
+	defer recovery()
+	targetRequest, alternateRequest, err := duplicateRequest(req)
+
+	// Alternate request
+	go func() {
+		defer recovery()
+		resp, err := request(*alternateHost, alternateRequest)
+		if err != nil && err != httputil.ErrPersistEOF {
+			log.Printf("Failed to receive from %s: %v\n", *alternateHost, err)
 		}
+		resp.Body.Close()
 	}()
 
-	// Open new TCP connection to the server
-	clientTcpConn, err := net.DialTimeout("tcp", h.Target, time.Duration(time.Duration(*productionTimeout)*time.Second))
-	if err != nil {
-		fmt.Printf("Failed to connect to %s\n", h.Target)
-		return
-	}
-	clientHttpConn := httputil.NewClientConn(clientTcpConn, nil) // Start a new HTTP connection on it
-	defer clientHttpConn.Close()                                 // Close the connection to the server
-	if *productionHostRewrite {
-		productionRequest.Host = h.Target
-	}
-	err = clientHttpConn.Write(productionRequest) // Pass on the request
-	if err != nil {
-		fmt.Printf("Failed to send to %s: %v\n", h.Target, err)
-		return
-	}
-	resp, err := clientHttpConn.Read(productionRequest) // Read back the reply
+	// Target request
+	resp, err := request(*targetHost, targetRequest)
 	if err != nil && err != httputil.ErrPersistEOF {
-		fmt.Printf("Failed to receive from %s: %v\n", h.Target, err)
+		log.Printf("Failed to receive from %s: %v\n", *targetHost, err)
 		return
 	}
-	defer resp.Body.Close()
+
 	for k, v := range resp.Header {
 		w.Header()[k] = v
 	}
 	w.WriteHeader(resp.StatusCode)
 	body, _ := ioutil.ReadAll(resp.Body)
 	w.Write(body)
+	resp.Body.Close()
 }
 
-func main() {
-	flag.Parse()
-
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	var err error
-
-	var listener net.Listener
-
-	if len(*tlsPrivateKey) > 0 {
-		cer, err := tls.LoadX509KeyPair(*tlsCertificate, *tlsPrivateKey)
-		if err != nil {
-			fmt.Printf("Failed to load certficate: %s and private key: %s", *tlsCertificate, *tlsPrivateKey)
-			return
-		}
-
-		config := &tls.Config{Certificates: []tls.Certificate{cer}}
-		listener, err = tls.Listen("tcp", *listen, config)
-		if err != nil {
-			fmt.Printf("Failed to listen to %s: %s\n", *listen, err)
-			return
-		}
-	} else {
-		listener, err = net.Listen("tcp", *listen)
-		if err != nil {
-			fmt.Printf("Failed to listen to %s: %s\n", *listen, err)
-			return
-		}
+func copyRequest(request *http.Request, body io.ReadCloser, rewriteHost bool, host string) *http.Request {
+	r := http.Request{
+		Method:        request.Method,
+		URL:           request.URL,
+		Proto:         request.Proto,
+		ProtoMajor:    request.ProtoMajor,
+		ProtoMinor:    request.ProtoMinor,
+		Header:        request.Header,
+		Body:          body,
+		Host:          request.Host,
+		ContentLength: request.ContentLength,
+		Close:         true,
 	}
 
-	h := handler{
-		Target:      *targetProduction,
-		Alternative: *altTarget,
-		Randomizer:  *rand.New(rand.NewSource(time.Now().UnixNano())),
+	if rewriteHost {
+		r.Host = host
 	}
-	http.Serve(listener, h)
+
+	return &r
 }
 
-type nopCloser struct {
-	io.Reader
-}
-
-func (nopCloser) Close() error { return nil }
-
-func DuplicateRequest(request *http.Request) (request1 *http.Request, request2 *http.Request) {
+func duplicateRequest(request *http.Request) (*http.Request, *http.Request, error) {
+	// Duplicate the request body.
 	b1 := new(bytes.Buffer)
 	b2 := new(bytes.Buffer)
 	w := io.MultiWriter(b1, b2)
-	io.Copy(w, request.Body)
-	defer request.Body.Close()
-	request1 = &http.Request{
-		Method:        request.Method,
-		URL:           request.URL,
-		Proto:         request.Proto,
-		ProtoMajor:    request.ProtoMajor,
-		ProtoMinor:    request.ProtoMinor,
-		Header:        request.Header,
-		Body:          nopCloser{b1},
-		Host:          request.Host,
-		ContentLength: request.ContentLength,
-		Close:         true,
+	_, err := io.Copy(w, request.Body)
+	request.Body.Close()
+
+	// Duplicate the request, using the duplicated body for each.
+	r1 := copyRequest(request, noopCloser{b1}, *targetHostRewrite, *targetHost)
+	r2 := copyRequest(request, noopCloser{b2}, *alternateHostRewrite, *alternateHost)
+
+	return r1, r2, err
+}
+
+// request invokes the request upon the host.
+func request(host string, r *http.Request) (*http.Response, error) {
+	tcpConn, err := net.DialTimeout("tcp", host, time.Duration(time.Duration(*alternateTimeout)*time.Second))
+	if err != nil {
+		return nil, err
 	}
-	request2 = &http.Request{
-		Method:        request.Method,
-		URL:           request.URL,
-		Proto:         request.Proto,
-		ProtoMajor:    request.ProtoMajor,
-		ProtoMinor:    request.ProtoMinor,
-		Header:        request.Header,
-		Body:          nopCloser{b2},
-		Host:          request.Host,
-		ContentLength: request.ContentLength,
-		Close:         true,
+
+	var resp *http.Response
+	conn := httputil.NewClientConn(tcpConn, nil)
+	err = conn.Write(r)
+	if err == nil {
+		resp, err = conn.Read(r)
 	}
-	return
+	conn.Close()
+	return resp, err
 }
